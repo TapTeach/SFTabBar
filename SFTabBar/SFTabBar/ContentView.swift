797ef4077a693f1ae4005ebaa7de7c4e691d8fe2
militantjones <militantjones@gmail.com>
2020-09-16 12:27:07 -0500

    Initial Project Setup

---
+++ SFTabBar/SFTabBar/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SFTabBar
+//
+//  Created by Adam Jones on 9/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SFTabBar/SFTabBar/SFTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFTabBarApp.swift
+//  SFTabBar
+//
+//  Created by Adam Jones on 9/16/20.
+//
+
+import SwiftUI
+
+@main
+struct SFTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
