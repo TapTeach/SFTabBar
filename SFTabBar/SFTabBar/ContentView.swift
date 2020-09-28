b6fc27d6e84738493e2bee530e0173ce6d4a8594
militantjones <militantjones@gmail.com>
2020-09-28 14:26:25 -0500

    added labels and icon class, color picker and share icon

--- SFTabBar/SFTabBar/ContentView.swift
+++ SFTabBar/SFTabBar/ContentView.swift
@@ -7,7 +7,22 @@
 
 import SwiftUI
 
-
+class TabsViewModel: ObservableObject {
+    @Published var tab1Label = "Dash"
+    @Published var tab1Icon = "gauge"
+    
+    @Published var tab2Label = "Trends"
+    @Published var tab2Icon = "flame"
+    
+    @Published var tab3Label = "Shop"
+    @Published var tab3Icon = "bag"
+    
+    @Published var tab4Label = "Profile"
+    @Published var tab4Icon = "person.crop.circle"
+    
+    @Published var tab5Label = "More"
+    @Published var tab5Icon = "ellipsis"
+}
 
 struct ContentView: View {
     
@@ -17,10 +32,10 @@ struct ContentView: View {
     @State private var tintColor = Color.red
     @State private var tbColor = Color.green
     
-    @State var isDarkMode = false
-    @State var quantity: Int = 3
-    @State var tab1Label: String = ""
+    @State var isWhiteHomeIndicator = false
+    @State var quantity: Int = 5
     
+    @ObservedObject var tabs = TabsViewModel()
     
     var body: some View {
         
@@ -28,23 +43,27 @@ struct ContentView: View {
             VStack {
                 ZStack {
                     ZStack {
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        RoundedRectangle(cornerRadius: 0, style: .continuous)
                             .fill(tbColor)
                         HStack(alignment: .top){
-                            SFTabBar.tabItem()
-                            SFTabBar.tabItem()
-                            SFTabBar.tabItem()
+                            SFTabBar.tabItem(icon: tabs.tab1Icon, label: tabs.tab1Label)
+                            SFTabBar.tabItem(icon: tabs.tab2Icon, label: tabs.tab2Label)
+                            SFTabBar.tabItem(icon: tabs.tab3Icon, label: tabs.tab3Label)
+                            SFTabBar.tabItem(icon: tabs.tab4Icon, label: tabs.tab4Label)
+                            SFTabBar.tabItem(icon: tabs.tab5Icon, label: tabs.tab5Label)
                         }
-                        .padding(.bottom)
+                        .padding([.leading, .bottom, .trailing])
+                        Image(isWhiteHomeIndicator ? "img_homeIndicator_white" : "img_homeIndicator_black" )
+                            .offset(y: 24.0)
                     }
-                    .frame(width: 320, height: 85)
-                    .offset(y: 52.0)
-                    Image("img_iphone11")
-                        
+                    .frame(width: 315, height: 74)
+                    .offset(y: 34.0)
+                    Image("device_iphone11")
+                        .offset(y: -10.0)
                 }.clipped()
                 Form {
                     Section(header: Text("Tab Bar Setting")) {
-                        Toggle(isOn: $isDarkMode) {
+                        Toggle(isOn: $isWhiteHomeIndicator) {
                             Text("White Home Indicator")
                         }
                         HStack {
@@ -65,37 +84,81 @@ struct ContentView: View {
                             ColorPicker("", selection: $tintColor, supportsOpacity: false)
                                 .frame(width: 40, alignment: .center)
                         }
-                        Stepper("\(quantity) Tabs ", value: $quantity, in: 0...5)
+                        Stepper("Display \(quantity) Tabs ", value: $quantity, in: 1...5)
                         
                     }
                     Section(header: Text("Tab 1")) {
-                        TextField("Tab Label", text: $tab1Label)
+                        TextField("Tab Label", text: $tabs.tab1Label)
                         NavigationLink("Select a SF Symbol", destination: Text("Detail").navigationBarTitle(Text("Select SFSymbol")))
                     }
                     Section(header: Text("Tab 2")) {
-                        TextField("Tab Label", text: $tab1Label)
+                        TextField("Tab Label", text: $tabs.tab2Label)
+                        NavigationLink("Select a SF Symbol", destination: Text("Detail").navigationBarTitle(Text("Select SFSymbol")))
+                    }
+                    Section(header: Text("Tab 3")) {
+                        TextField("Tab Label", text: $tabs.tab3Label)
+                        NavigationLink("Select a SF Symbol", destination: Text("Detail").navigationBarTitle(Text("Select SFSymbol")))
+                    }
+                    Section(header: Text("Tab 4")) {
+                        TextField("Tab Label", text: $tabs.tab4Label)
+                        NavigationLink("Select a SF Symbol", destination: Text("Detail").navigationBarTitle(Text("Select SFSymbol")))
+                    }
+                    Section(header: Text("Tab 5")) {
+                        TextField("Tab Label", text: $tabs.tab5Label)
                         NavigationLink("Select a SF Symbol", destination: Text("Detail").navigationBarTitle(Text("Select SFSymbol")))
                     }
                 }
                 .navigationBarTitle("SF TabBar")
+                .navigationBarItems(trailing:
+                    Button(action: {
+                        print("Share Me!")
+                    }) {
+                        Image(systemName: "square.and.arrow.up").imageScale(.large)
+                            .foregroundColor(.pink)
+                    }
+                )
             }
         }
+        .accentColor(.pink)
         
     }
 }
 
 struct tabItem: View {
-
     
+    var icon: String
+    var label: String
+        
     var body: some View {
         VStack{
-            Image(systemName: "hare")
+            Image(systemName: icon)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 16.0, height: 16.0)
                 .foregroundColor(Color.white)
-            Text("Label")
-                .font(.caption2)
+            Text(label)
                 .foregroundColor(Color.white)
+                .font(.system(size: 9))
+                .lineLimit(1)
                
         } .frame(maxWidth: .infinity)
+    }
+}
+
+extension UINavigationController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        let standardAppearance = UINavigationBarAppearance()
+        standardAppearance.backgroundColor = .black
+        standardAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        let scrollEdgeAppearance = UINavigationBarAppearance()
+        scrollEdgeAppearance.backgroundColor = .black
+        scrollEdgeAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+        navigationBar.standardAppearance = standardAppearance
+        navigationBar.scrollEdgeAppearance = scrollEdgeAppearance
     }
 }
 
